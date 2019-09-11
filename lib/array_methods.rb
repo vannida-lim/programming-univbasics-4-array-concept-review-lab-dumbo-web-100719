e0e30aed32c5c1935b93049cdd2e690d04ef36d8
Vannida Lim <vannida731@gmail.com>
2019-09-11 05:48:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,10 +12,12 @@ end
 def find_max_value(array)
   current_max = array[0]
   array.length.times { |index|
-    puts array[index]
+    current_element = array[index]
+    if current_max < current_element
+      current_max = current_element
   }
   end
-  
+  return current_max
 end
 
 
