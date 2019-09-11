c379da19557dc901bbd3f261c354aaf029de66e0
Vannida Lim <vannida731@gmail.com>
2019-09-11 05:54:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,9 @@ def find_element_index(array, value_to_find)
   index = 0 
   while index < array.length do
     if array[index] == value_to_find
-      index
+      return index
      end
+     index += 1
   end
   nil
 end
